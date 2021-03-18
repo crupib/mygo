6ce7e7b78bd56cb5572582d3e38344d66426d56b
bill <crupib@gmail.com>
2021-03-18 10:25:56 -0400

    plural_sight: :

--- src/plural_sight/main.go
+++ src/plural_sight/main.go
@@ -1,9 +1,16 @@
 package main
 
-import ( 
+import (
 	"fmt"
+)
+
+const (
+	first  = iota
+	second = iota
 )
 
 func main() {
 	fmt.Println("hello world")
+	fmt.Println(first)
+	fmt.Println(second)
 }
