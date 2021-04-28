f00fdced3ab92e9b314993f3068f06884b7aff0b
bill <crupib@gmail.com>
2021-04-28 08:37:21 -0400

    controllers

--- src/plural_sight/code/controllers/users.go
+++ src/plural_sight/code/controllers/users.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"net/http"
+	"plural_sight/code/models"
 	"regexp"
 )
 
@@ -12,7 +13,20 @@ type userController struct {
 func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from User Controller!"))
 }
-
+func (uc *userController) getAll(w http.ResponseWriter, r *http.Request) {
+	encodeResponseAsJSON(models.GetUsers(),2)
+}
+func (uc *userController) get(id int, w http.ResponseWriter) {
+	u, err := models.GetUserByID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		encodeResponseAsJSON(u, w)
+	}
+}
+func (uc *userController) post(w http.ResponseWriter, r *http.Request) {
+	u, err := uc.parseRequest(r)
+	if err 
+}
 func newUserController () *userController {
 	return &userController {
 		userIDPattern: regexp.MustCompile(`^/users/(\d+)/?`),
