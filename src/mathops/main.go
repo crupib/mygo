f3010cb2848e57aede977654b79d1ebd10b5a1c7
bill <crupib@gmail.com>
2021-08-10 09:06:57 -0400

    LL class

--- src/mathops/main.go
+++ src/mathops/main.go
@@ -1,11 +1,6 @@
-package main
 
-import (
-	"fmt"
-	"math"
-)
 
-func main() {
+/*
 	var anInt int = 5
 	var aFloat float64 = 42
 	sum := float64(anInt) + aFloat
@@ -18,4 +13,16 @@ func main() {
 	var aFloat3 = math.Pi
 	var rounded2 = math.Round(aFloat3)
 	fmt.Printf("Original: %v, Rounded: %v\n", aFloat3, rounded2)
+*/
+	i1, i2, i3 := 12,45,68
+	intSum := i1 + i2 + i3
+	fmt.Println("Integer sum",intSum)
+	f1, f2, f3 := 23.5, 65.1, 76.3
+	floatSum := f1 + f2 + f3
+	fmt.Println("Float sum",floatSum)
+	floatSum = math.Round(floatSum * 100) / 100
+	fmt.Println("The sum is now",floatSum)
+	circleRadius := 15.5
+	circumference := circleRadius* 2 * math.Pi
+	fmt.Printf("Circumference: %.2f\n",circumference)
 }
