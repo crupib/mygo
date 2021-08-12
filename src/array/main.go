0d0a171a3b69b1f0f9d5bfe62c34d747cb7d79f6
bill <crupib@gmail.com>
2021-08-11 22:06:09 -0400

    go arrays

--- src/array/main.go
+++ src/array/main.go
@@ -1,31 +1,19 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"math"
-	"os"
-	"strconv"
-	"strings"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Value 1: ")
-	input1, _ := reader.ReadString('\n')
-	float1, err := strconv.ParseFloat(strings.TrimSpace(input1),64)
-	if err != nil {
-		fmt.Println(err)
-		panic("Value 1 must be a number")
-	}
-	fmt.Print("Value 2: ")
-	input2, _ := reader.ReadString('\n')
-	float2, err := strconv.ParseFloat(strings.TrimSpace(input2),64)
-	if err != nil {
-		fmt.Println(err)
-		panic("Value 2 must be a number")
-	}
-    sum := float1 + float2
-    sum = math.Round(sum*100) / 100   
-    fmt.Printf("The sum of %v and %v is %v\n\n",float1, float2,sum)
+	var colors [3]string
+	colors[0] = "Red"
+	colors[1] = "Green"
+	colors[2] = "Blue"
+	fmt.Println(colors)
+	fmt.Println(colors[0])
+	var numbers = [5]int{5,3,1,2,4}
+	fmt.Println(numbers)
+	fmt.Println("Nubers of colors:",len(colors))
+	fmt.Println("Nubers of numbers:",len(numbers))
+	
 }
