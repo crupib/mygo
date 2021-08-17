70cd81718435c3f88e7dd3ea0855cea96e04e347
bill <crupib@gmail.com>
2021-08-17 16:16:42 -0400

    calc and struct with methods

--- src/calculator/main.go
+++ src/calculator/main.go
@@ -3,30 +3,65 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Value 1: ")
-	input1, _ := reader.ReadString('\n')
-	float1, err := strconv.ParseFloat(strings.TrimSpace(input1),64)
-	if err != nil {
-		fmt.Println(err)
-		panic("Value 1 must be a number")
-	}
-	fmt.Print("Value 2: ")
-	input2, _ := reader.ReadString('\n')
-	float2, err := strconv.ParseFloat(strings.TrimSpace(input2),64)
-	if err != nil {
-		fmt.Println(err)
-		panic("Value 2 must be a number")
-	}
-    sum := float1 + float2
-    sum = math.Round(sum*100) / 100   
-    fmt.Printf("The sum of %v and %v is %v\n\n",float1, float2,sum)
+var reader = bufio.NewReader(os.Stdin)
 
+func main() {
+
+	value1 := getInput("Value 1")
+	value2 := getInput("Value 2")
+
+	var result float64
+
+	switch operation := getOperation(); operation {
+	case "+":
+		result = addValues(value1, value2)
+	case "-":
+		result = subtractValues(value1, value2)
+	case "*":
+		result = multiplyValues(value1, value2)
+	case "/":
+		result = divideValues(value1, value2)
+	default:
+		panic("Invalid operation")
+	}
+	fmt.Printf("The result is %v\n\n", result)
+
+}
+
+func getInput(prompt string) float64 {
+	fmt.Printf("%v: ", prompt)
+	input, _ := reader.ReadString('\n')
+	value, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+	if err != nil {
+		message := fmt.Sprintf("%v must be a number", prompt)
+		panic(message)
+	}
+	return value
+}
+
+func getOperation() string {
+	fmt.Print("Select an operation (+ - * /): ")
+	op, _ := reader.ReadString('\n')
+	return strings.TrimSpace(op)
+}
+
+func addValues(value1, value2 float64) float64 {
+	return value1 + value2
+}
+
+func subtractValues(value1, value2 float64) float64 {
+	return value1 - value2
+}
+
+func multiplyValues(value1, value2 float64) float64 {
+	return value1 * value2
+}
+
+func divideValues(value1, value2 float64) float64 {
+	return value1 / value2
 }
--- src/struct/main.go
+++ src/struct/main.go
@@ -4,16 +4,29 @@ import (
 	"fmt"
 )
 func main() {
-	fmt.Println("Structures")
-	poodle := Dog{"Poodle", 10}
-	fmt.Println(poodle)
-	fmt.Printf("%+v\n",poodle)
-	fmt.Printf("Breed: %v\nWeight: %v\n",poodle.Breed,poodle.Weight)
+//	fmt.Println("Structures")
+	poodle := Dog{"Poodle", 10, "Woof!"}
+//	fmt.Println(poodle)
+//	fmt.Printf("%+v\n",poodle)
+//	fmt.Printf("Breed: %v\nWeight: %v\nSound: %v\n", poodle.Breed, poodle.Weight, poodle.Sound)
 	poodle.Weight = 9
-	fmt.Printf("Breed: %v\nWeight: %v\n",poodle.Breed,poodle.Weight)
+	fmt.Printf("Breed: %v\nWeight: %v\nSound: %v\n", poodle.Breed, poodle.Weight, poodle.Sound)
+	poodle.Speak()
+	poodle.Sound = "Arf!"
+	poodle.SpeakThreeTimes()
 }
 // Dog is a struct
 type Dog struct {
 	Breed string
 	Weight int
+	Sound string
+}
+// Speak is how the dog speaks
+func (d Dog) Speak(){
+	fmt.Println(d.Sound)
+}
+// SpeakThreeTimes Speak three times
+func (d Dog) SpeakThreeTimes() {
+	d.Sound = fmt.Sprintf("%v %v %v", d.Sound, d.Sound, d.Sound)
+	fmt.Println(d.Sound)
 }
