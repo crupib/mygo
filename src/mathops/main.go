3f13690420cb27e3c8e14e4cebf63cd50dcde517
bill <crupib@gmail.com>
2022-01-21 16:37:50 -0500

    go intro

--- src/mathops/main.go
+++ src/mathops/main.go
@@ -1,5 +1,3 @@
-
-
 /*
 	var anInt int = 5
 	var aFloat float64 = 42
@@ -14,15 +12,25 @@
 	var rounded2 = math.Round(aFloat3)
 	fmt.Printf("Original: %v, Rounded: %v\n", aFloat3, rounded2)
 */
-	i1, i2, i3 := 12,45,68
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// this is a comment
+
+func main() {
+	i1, i2, i3 := 12, 45, 68
 	intSum := i1 + i2 + i3
-	fmt.Println("Integer sum",intSum)
+	fmt.Println("Integer sum", intSum)
 	f1, f2, f3 := 23.5, 65.1, 76.3
 	floatSum := f1 + f2 + f3
-	fmt.Println("Float sum",floatSum)
-	floatSum = math.Round(floatSum * 100) / 100
-	fmt.Println("The sum is now",floatSum)
+	fmt.Println("Float sum", floatSum)
+	floatSum = math.Round(floatSum*100) / 100
+	fmt.Println("The sum is now", floatSum)
 	circleRadius := 15.5
-	circumference := circleRadius* 2 * math.Pi
-	fmt.Printf("Circumference: %.2f\n",circumference)
+	circumference := circleRadius * 2 * math.Pi
+	fmt.Printf("Circumference: %.2f\n", circumference)
 }
